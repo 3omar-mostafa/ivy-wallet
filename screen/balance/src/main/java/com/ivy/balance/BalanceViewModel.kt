96c792a703f147731dcf62e1de0d9d2a1dd0bf66
Omar Mostafa <42312059+3omar-mostafa@users.noreply.github.com>
2025-06-04 01:45:34 +0300

    Fixed Balance After Planned Payments Calculations

--- screen/balance/src/main/java/com/ivy/balance/BalanceViewModel.kt
+++ screen/balance/src/main/java/com/ivy/balance/BalanceViewModel.kt
@@ -11,9 +11,13 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.viewModelScope
 import com.ivy.base.time.TimeConverter
 import com.ivy.base.time.TimeProvider
+import com.ivy.legacy.data.model.FromToTimeRange
+import com.ivy.legacy.data.model.Month
 import com.ivy.ui.ComposeViewModel
 import com.ivy.legacy.data.model.TimePeriod
+import com.ivy.legacy.utils.atEndOfDay
 import com.ivy.legacy.utils.ioThread
+import com.ivy.legacy.utils.withDayOfMonthSafe
 import com.ivy.wallet.domain.action.settings.BaseCurrencyAct
 import com.ivy.wallet.domain.action.wallet.CalcWalletBalanceAct
 import com.ivy.wallet.domain.deprecated.logic.PlannedPaymentsLogic
@@ -37,12 +41,11 @@ class BalanceViewModel @Inject constructor(
     private var currentBalance by mutableDoubleStateOf(0.0)
     private var plannedPaymentsAmount by mutableDoubleStateOf(0.0)
     private var balanceAfterPlannedPayments by mutableDoubleStateOf(0.0)
-    private var numberOfMonthsAhead by mutableIntStateOf(1)
 
     @Composable
     override fun uiState(): BalanceState {
         LaunchedEffect(Unit) {
-            start()
+            start(timePeriod = fixTimePeriod(ivyContext.selectedPeriod, 0))
         }
 
         return BalanceState(
@@ -63,7 +66,7 @@ class BalanceViewModel @Inject constructor(
     }
 
     private fun start(
-        timePeriod: TimePeriod = ivyContext.selectedPeriod
+        timePeriod: TimePeriod
     ) {
         viewModelScope.launch {
             baseCurrencyCode = baseCurrencyAct(Unit)
@@ -74,43 +77,47 @@ class BalanceViewModel @Inject constructor(
             ).toDouble()
 
             plannedPaymentsAmount = ioThread {
-                plannedPaymentsLogic.plannedPaymentsAmountFor(
-                    timePeriod.toRange(ivyContext.startDayOfMonth, timeConverter, timeProvider)
-                    // + positive if Income > Expenses else - negative
-                ) * if (numberOfMonthsAhead >= 0) {
-                    numberOfMonthsAhead.toDouble()
-                } else {
-                    1.0
-                }
+                val range = timePeriod.fromToRange ?: timePeriod.toRange(ivyContext.startDayOfMonth, timeConverter, timeProvider)
+                plannedPaymentsLogic.plannedPaymentsAmountFor(range)
             }
             balanceAfterPlannedPayments =
                 currentBalance + plannedPaymentsAmount
+
+            if (period.month != null) {
+                period = TimePeriod(month = period.month, year = period.year)
+            }
         }
     }
 
     private fun setTimePeriod(timePeriod: TimePeriod) {
-        start(timePeriod = timePeriod)
+        start(timePeriod = fixTimePeriod(timePeriod, 0))
     }
 
     private fun nextMonth() {
-        val month = period.month
-        val year = period.year ?: com.ivy.legacy.utils.dateNowUTC().year
-        numberOfMonthsAhead += 1
-        if (month != null) {
-            start(
-                timePeriod = month.incrementMonthPeriod(ivyContext, 1L, year = year)
-            )
-        }
+        start(timePeriod = fixTimePeriod(period, 1))
     }
 
     private fun previousMonth() {
-        val month = period.month
-        val year = period.year ?: com.ivy.legacy.utils.dateNowUTC().year
-        numberOfMonthsAhead -= 1
+        start(timePeriod = fixTimePeriod(period, -1))
+    }
+
+    private fun fixTimePeriod(timePeriod: TimePeriod, plusMonths: Long): TimePeriod {
+        var month = timePeriod.month
+        var year = timePeriod.year ?: com.ivy.legacy.utils.dateNowUTC().year
+
         if (month != null) {
-            start(
-                timePeriod = month.incrementMonthPeriod(ivyContext, -1L, year = year)
-            )
+            val to = month.toDate().withYear(year)
+                .plusMonths(plusMonths + 1)
+                .withDayOfMonthSafe(1)
+                .minusDays(1)
+                .atEndOfDay()
+
+            month = Month.fromMonthValue(to.monthValue)
+            year = to.year
+
+            val instant = to.atZone(timeProvider.getZoneId()).toInstant()
+            return TimePeriod(fromToRange = FromToTimeRange(from = null, to = instant), month = month, year = year)
         }
+        return timePeriod
     }
 }
